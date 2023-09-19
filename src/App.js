d48de366d8facb07eed4f1be664ed1fbe36ee5ca
CrisjohnOfficial <124770859+CrisTheNoob@users.noreply.github.com>
2023-09-19 20:25:17 +0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch master
    Changes to be committed:
    	modified:   src/App.css
    	modified:   src/App.js
    	deleted:    src/logo.svg

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
+  const display = document.querySelector(".display");
+  const buttons = document.querySelectorAll("button");
+  const specialChars = ["%", "*", "/", "-", "+", "="];
+  let output = "";
+  
+  const calculate = (btnValue) => {
+      if(btnValue === "=" && output !== ""){
+          output = eval(output.replace("%", "/100"));
+      }else if(btnValue === "AC"){
+          output = ""
+      }else if(btnValue === "DEL"){
+          output = output.toString().slice(0, -1);
+      }else{
+          if(output === "" && specialChars.includes(btnValue)) return;
+          output += btnValue;
+      }
+   display.value = output;
+  }
+  
+  buttons.forEach((button) =>{
+      button.addEventListener("click", e => calculate(e.target.dataset.value));
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+         <input type="text" class="display"/>
+
+        <div class="buttons">
+            <button class="operator" data-value="AC">AC</button>
+            <button class="operator" data-value="DEL">DEL</button>
+            <button class="operator" data-value="%">%</button>
+            <button class="operator" data-value="/">/</button>
+
+            <button data-value="7">7</button>
+            <button data-value="8">8</button>
+            <button data-value="9">9</button>
+            <button class="operator" data-value="*">*</button>
+
+            <button data-value="4">4</button>
+            <button data-value="5">5</button>
+            <button data-value="6">6</button>
+            <button class="operator" data-value="-">-</button>
+
+            <button data-value="1">1</button>
+            <button data-value="2">2</button>
+            <button data-value="3">3</button>
+            <button class="operator" data-value="+">+</button>
+
+            <button data-value="0">0</button>
+            <button data-value="00">00</button>
+            <button data-value=".">.</button>
+            <button class="operator" data-value="=">=</button>
+
+        </div>
+    </div>
     </div>
   );
 }
